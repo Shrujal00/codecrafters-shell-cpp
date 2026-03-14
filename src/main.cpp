f1d6e028562749edfa1fa13718092b0b6fe1bf6c
Shrujal <shrujal@example.com>
2026-03-14 11:56:36 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,14 @@ int main() {
     std::cerr << std::unitbuf;
     std::cout << "$ ";
     std::string input;
+    std::getline(std::cin, input);
 
     if (input == "exit")
     {
       break;
     }
+
     
-    std::getline(std::cin, input);
     std::cout << input << ": command not found" << "\n";
   }  
 }
