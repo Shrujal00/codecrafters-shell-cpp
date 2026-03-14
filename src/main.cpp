08cc3626e9d0033cf189fa6f4c769d1363991808
Shrujal00 <shrujal000@gmail.com>
2026-03-14 07:41:30 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  while(true) 
+int main()
+{
+  std::string builtin[] = {"type", "echo", "exit"};
+  while (true)
   {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -10,19 +12,43 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    // exit
     if (input == "exit")
     {
       break;
     }
 
-    else if (input.substr(0,4) == "echo")
+    // echo
+    else if (input.substr(0, 4) == "echo")
     {
+      if (input == "echo")
+      {
+        std::cout << "please type something !\n";
+        continue;
+      }
       std::cout << input.substr(5) << std::endl;
     }
 
-    else 
+    // type
+    else if (input.substr(0, 4) == "type")
+    {
+      if(input == "type")
+      {
+        std::cout << "Wrong usage: type `command_name`";
+      }
+      for (int i = 0; i < builtin->size(); i++)
+      {
+        if (input.substr(5) == builtin[i])
+        {
+          std::cout << builtin[i] << " is a shell builtin" << std::endl;
+        }
+      }
+      std::cout << input.substr(5) << ": not found" << std::endl;
+    }
+
+    else
     {
       std::cout << input << ": command not found" << "\n";
     }
-  }  
+  }
 }
